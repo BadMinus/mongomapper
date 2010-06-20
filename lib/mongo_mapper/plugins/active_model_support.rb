520850b003c047a9b0fba5b1f266ec0c239ec07e
Onemanstartup <onemanstartup@gmail.com>
2010-06-20 14:22:56 +0400

    fix typo

---
+++ lib/mongo_mapper/plugins/active_model_support.rb
@@ -0,0 +1,16 @@
+module MongoMapper
+  module Plugins
+    module ActiveModelSupport
+      def self.configure(model)
+        model.class_eval do
+          include ActiveModel::Serializers::Xml
+          include ActiveModel::Conversion
+          include ActiveModel::Naming
+          include ActiveModel::Serialization
+          include ActiveModel::Serializers::JSON
+          model.extend ActiveModel::Translation  if defined?(ActiveModel)
+        end
+      end
+    end
+  end
+end
